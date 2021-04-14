b698d1a30eef6b1280f2d3bff3266bd3efa635ca
ron2702 <rn2702@gmail.com>
2021-04-14 15:01:49 -0400

    React Course

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+ 
+    return(
+        <div className="expenses">
+            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}></ExpenseItem>
+            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}></ExpenseItem>
+        </div>
+    );
+    
+}
+
+export default Expenses;
